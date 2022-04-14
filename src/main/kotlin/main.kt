7faea855c5be4695fb478005d671ffb44efb3c29
jud-ie19 <owuorakinyijudith@gmail.com>
2022-04-14 09:28:33 +0300

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,65 @@
+fun main(){
+ CurrentAccount(10875436,"Owuorj",5000)
+
+
+
+
+
+
+
+
+}
+ class CurrentAccount(var accountnumber:Int,var accountname:String,var balance:Int) {
+
+fun deposit(amount:Double){
+    balance+=deposit()
+    println(balance)
+
+
+
+
+
+    }
+  fun widthdraw(amount: Double){
+     var x=-
+     println(x)
+
+    }
+  fun details(){
+   println("account number x with balance y is operated with z")
+
+  }
+
+ }
+class SavingAccount(var accountnumber:Int,var accountname:String,var balance:Int){
+ fun deposit(amount:Double){
+
+ }
+
+
+
+
+
+
+
+ fun details(){
+  println("account number x with balance y is operated with z")
+
+ }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
